a9c38111a66165ca361e357f1116817db30c3962
Tal Yaron <tal.yaron@gmail.com>
2024-12-01 10:23:21 +0200

    delib-sign - first count working well

--- functions/src/fn_signatures.ts
+++ functions/src/fn_signatures.ts
@@ -1,10 +1,11 @@
 import { db } from "./index";
 import { logger } from "firebase-functions/v1";
 import { getAction } from "./fn_approval";
-import { Collections, DocumentSigns, Signature } from "delib-npm";
+import { Collections, DocumentSigns, Signature, SignatureType } from "delib-npm";
 
 //functions for FreeDi Sign
-
+//TODO: deploy this function
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function updateDocumentSignatures(ev: any) {
 	try {
 
@@ -12,7 +13,7 @@ export function updateDocumentSignatures(ev: any) {
 		const signatureAfterData = ev.data.after.data() as Signature;
 		const signatureBeforeData = ev.data.before.data() as Signature;
 		const signatureData = signatureAfterData || signatureBeforeData;
-
+console.log("action", action);
 		switch (action) {
 		case "create":
 			onCreateSignature(signatureData);
@@ -34,7 +35,7 @@ export function updateDocumentSignatures(ev: any) {
 
 async function onCreateSignature(signature: Signature) {
 	try {
-		const { documentId, signed, levelOfSignature } = signature;
+		const { documentId, signed, levelOfSignature } = signature as Signature;
 
 		await db.runTransaction(async (transaction) => {
 			const documentSignatureRef = db.collection(Collections.documentsSigns)
@@ -46,18 +47,18 @@ async function onCreateSignature(signature: Signature) {
 				const documentSignature: DocumentSigns = {
 					documentId,
 					viewed: 1,
-					signed: signed ? 1 : 0,
-					rejected: signed ? 0 : 1,
-					avgSignatures: signed ? levelOfSignature : 0,
-					totalSignaturesLevel: signed ? levelOfSignature : 0
+					signed: signed === SignatureType.signed ? 1 : 0,
+					rejected: signed === SignatureType.rejected ? 1 : 0,
+					avgSignatures: signed === SignatureType.signed ? levelOfSignature : 0,
+					totalSignaturesLevel: signed === SignatureType.signed ? levelOfSignature : 0
 				};
 				transaction.set(documentSignatureRef, documentSignature);
 			} else {
 				const documentSignature = documentSignatureDB.data() as DocumentSigns;
 				documentSignature.viewed += 1;
-				documentSignature.signed += signed ? 1 : 0;
-				documentSignature.rejected += signed ? 0 : 1;
-				documentSignature.totalSignaturesLevel += signed ? levelOfSignature : 0;
+				documentSignature.signed += signed === SignatureType.signed ? 1 : 0;
+				documentSignature.rejected += signed  === SignatureType.rejected? 1 : 0;
+				documentSignature.totalSignaturesLevel += signed === SignatureType.signed ? levelOfSignature : 0;
 				documentSignature.avgSignatures = documentSignature.totalSignaturesLevel / documentSignature.viewed;
 				transaction.update(documentSignatureRef, documentSignature);
 
@@ -71,7 +72,7 @@ async function onCreateSignature(signature: Signature) {
 
 async function onDeleteSignature(signature: Signature) {
 	try {
-		const { documentId, signed, levelOfSignature } = signature;
+		const { documentId, signed, levelOfSignature } = signature as Signature;
 
 		await db.runTransaction(async (transaction) => {
 			const documentSignatureRef = db.collection(Collections.documentsSigns)
@@ -81,9 +82,9 @@ async function onDeleteSignature(signature: Signature) {
 			if (documentSignatureDB.exists) {
 				const documentSignature = documentSignatureDB.data() as DocumentSigns;
 				documentSignature.viewed -= 1;
-				documentSignature.signed -= signed ? 1 : 0;
-				documentSignature.rejected -= signed ? 0 : 1;
-				documentSignature.totalSignaturesLevel -= signed ? levelOfSignature : 0;
+				documentSignature.signed -= signed === SignatureType.signed ? 1 : 0;
+				documentSignature.rejected -= signed === SignatureType.rejected ? 0 : 1;
+				documentSignature.totalSignaturesLevel -= signed === SignatureType.signed ? levelOfSignature : 0;
 				documentSignature.avgSignatures = documentSignature.totalSignaturesLevel / documentSignature.viewed;
 				transaction.update(documentSignatureRef, documentSignature);
 			}
@@ -99,8 +100,11 @@ async function onUpdateSignature(signatureBeforeData: Signature, signatureAfterD
 		const { signed: signedBefore, levelOfSignature: levelOfSignatureBefore } = signatureBeforeData;
 		const { documentId, signed: signedAfter, levelOfSignature: levelOfSignatureAfter } = signatureAfterData;
 
-		const diffSigned = (signedAfter ? 1 : 0) - (signedBefore ? 1 : 0);
-		const diffRejected = (signedAfter ? 0 : 1) - (signedBefore ? 0 : 1);
+		const diffSigned = (signedAfter === SignatureType.signed ? 1 : 0) - (signedBefore === SignatureType.signed ? 1 : 0);
+		const diffRejected = (signedAfter === SignatureType.rejected ? 1 : 0) - (signedBefore === SignatureType.rejected ? 1 : 0);
+		console.log("signedBefore",signedBefore,  "signedAfter", signedAfter);
+		console.log("diffSigned",diffSigned,  "diffRejected", diffRejected);
+		console.log("levelOfSignatureBefore",levelOfSignatureBefore,  "levelOfSignatureAfter", levelOfSignatureAfter);
 		const diffSignatureLevel = levelOfSignatureAfter - levelOfSignatureBefore;
 
 		await db.runTransaction(async (transaction) => {
