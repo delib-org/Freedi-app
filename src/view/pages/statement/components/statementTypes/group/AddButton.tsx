c6e3fa5f5ccd4bfb3bac3b992367d2cf9af103d3
Elizigi <elizigi876@gmail.com>
2025-02-06 19:25:55 +0200

    feature/add-question-fab

--- src/view/pages/statement/components/statementTypes/group/AddButton.tsx
+++ src/view/pages/statement/components/statementTypes/group/AddButton.tsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import IconButton from '@/view/components/iconButton/IconButton';
 import PlusIcon from "@/assets/icons/plusIcon.svg?react";
 import AddDocumentIcon from "@/assets/icons/document.svg?react";
 import AddClusterIcon from "@/assets/icons/net-clusters.svg?react";
+import AddSubQuestionIcon from "@/assets/icons/questionIcon.svg?react";
+
 import AddSubGroupIcon from "@/assets/icons/team-group.svg?react";
+import { StatementContext } from '../../../StatementCont';
+import { StatementType } from '@/types/enums';
+import { QuestionType } from '@/types/question';
 
 export default function AddButton() {
 	const [actionsOpen, setActionsOpen] = React.useState(false)
 
+	const { handleSetNewStatement, setNewStatementType, setNewQuestionType } =
+		useContext(StatementContext);
+
+	function handleAddStatement(newStatementType: StatementType, questionType?: QuestionType) {
+
+		setNewStatementType(newStatementType);
+		if (questionType) {
+			setNewQuestionType(questionType);
+
+		}
+		handleSetNewStatement(true);
+	}
 	const onclick = () => {
 		setActionsOpen(!actionsOpen)
 	}
 
 	const addDocumentAction = () => {
-		return;
+		handleAddStatement(StatementType.question, QuestionType.multiStage)
 	}
 
 	const addClusterAction = () => {
-		return;
+		handleAddStatement(StatementType.question, QuestionType.massConsensus)
 	}
 
 	const addSubGroupAction = () => {
-		return;
+		handleAddStatement(StatementType.group)
+	}
+	const addSubQuestionAction = () => {
+		handleAddStatement(StatementType.question, QuestionType.singleStep)
 	}
 
 	return (
@@ -35,6 +55,9 @@ export default function AddButton() {
 				</IconButton>
 				<IconButton onClick={addSubGroupAction} className="action-btn">
 					<AddSubGroupIcon />
+				</IconButton>
+				<IconButton onClick={addSubQuestionAction} className="action-btn">
+					<AddSubQuestionIcon />
 				</IconButton>
 				<button className='invisibleBackground' onClick={() => setActionsOpen(false)}></button>
 			</>}
