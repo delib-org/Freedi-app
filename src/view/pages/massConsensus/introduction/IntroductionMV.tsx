633c3caaf5f5b5ed0ff23cc02a71089c32d36d28
Tal Yaron <tal.yaron@gmail.com>
2025-02-09 17:32:14 +0200

    working

--- src/view/pages/massConsensus/introduction/IntroductionMV.tsx
+++ src/view/pages/massConsensus/introduction/IntroductionMV.tsx
@@ -1,11 +1,10 @@
-import { Statement } from "delib-npm";
+import { Statement } from "@/types/statement";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 export function useIntroductionMV() {
 
 	const { statementId } = useParams<{ statementId: string }>();
-	console.log(statementId)
 
 	const [statement, setStatement] = useState<Statement | null>(null);
 	const [loading, setLoading] = useState(true);
@@ -19,7 +18,6 @@ export function useIntroductionMV() {
 			`https://massConsensusGetInitialData-qeesi7aziq-uc.a.run.app`;
 		const localEndPoint = `http://localhost:5001/delib-v3-dev/us-central1/massConsensusGetInitialData`;
 		const requestUrl = (location.hostname !== 'localhost') ? prodEndPoint : localEndPoint;
-		console.log(requestUrl)
 
 		fetch(`${requestUrl}?statementId=${statementId}`)
 			.then(res => res.json())
