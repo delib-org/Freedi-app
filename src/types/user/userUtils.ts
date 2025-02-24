61decf074e9930b2da43d1ef07dd08c420a4afbe
Vladislav <vbykanov1989@outlook.com>
2025-02-24 15:16:03 +0100

    Stop tracking firestore-debug.log

--- src/types/user/userUtils.ts
+++ src/types/user/userUtils.ts
@@ -2,7 +2,7 @@ import { User } from 'firebase/auth';
 
 export function convertFirebaseUserToCreator(user: User) {
 	return {
-		displayName: user.displayName,
+		displayName: user.displayName ?? 'Anonymous' + user.uid,
 		photoURL: user.photoURL,
 		uid: user.uid,
 	};
