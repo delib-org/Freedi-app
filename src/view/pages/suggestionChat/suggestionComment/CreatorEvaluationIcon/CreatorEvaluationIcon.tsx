6263b37e5c1f389185ade15ad636017b28f369d3
Tal Yaron <tal.yaron@gmail.com>
2025-03-12 04:16:17 +0200

    we have sugeestionComment-comments

--- src/view/pages/suggestionChat/suggestionComment/CreatorEvaluationIcon/CreatorEvaluationIcon.tsx
+++ src/view/pages/suggestionChat/suggestionComment/CreatorEvaluationIcon/CreatorEvaluationIcon.tsx
@@ -1,25 +1,23 @@
 import { enhancedEvaluationsThumbs } from "../../../statement/components/evaluations/components/evaluation/enhancedEvaluation/EnhancedEvaluationModel"
 import { FC } from 'react'
-import styles from './CreatorEvalautionIcon.module.scss';
-
-import { Statement } from "delib-npm"
-
+import styles from './CreatorEvaluationIcon.module.scss';
 
 interface Props {
 	evaluationNumber: number,
-	statement: Statement
 }
 
-const CreatorEvaluationIcon: FC<Props> = ({ evaluationNumber, statement }) => {
+const CreatorEvaluationIcon: FC<Props> = ({ evaluationNumber }) => {
 	const len = enhancedEvaluationsThumbs.length;
 	const normalizedNumber = len - (evaluationNumber + 1) * (len - 1) / 2 - 1
 
 	const thumb = enhancedEvaluationsThumbs[Math.round(normalizedNumber)];
 	if (!thumb) return null
-	console.log(thumb.colorSelected)
 
-	console.log("normalizedNumber", normalizedNumber, statement.statement)
-	return (<div className={styles.evaluationThumb} style={{ backgroundColor: thumb.colorSelected }}><img src={thumb.svg} alt="" /></div>)
+	return (<div
+		className={styles.evaluationThumb}
+		style={{ backgroundColor: thumb.colorSelected }}>
+		<img src={thumb.svg} alt="" />
+	</div>)
 }
 
 export default CreatorEvaluationIcon
--- src/view/pages/suggestionChat/suggestionComment/SuggestionComment.tsx
+++ src/view/pages/suggestionChat/suggestionComment/SuggestionComment.tsx
@@ -1,9 +1,13 @@
-import { Statement } from 'delib-npm';
+import { Statement, StatementType } from 'delib-npm';
 import styles from './SuggestionComment.module.scss';
-import { FC } from 'react';
+import { FC, KeyboardEvent, useEffect } from 'react';
 import { useSuggestionComment } from './SuggestionCommentMV';
-import CreatorEvaluationIcon from './creatorEvalautionIcon/CreatorEvalautionIcon';
-
+import CreatorEvaluationIcon from './CreatorEvaluationIcon/CreatorEvaluationIcon';
+import { saveStatementToDB } from '@/controllers/db/statements/setStatements';
+import { useSelector } from 'react-redux';
+import { statementSubsSelector } from '@/redux/statements/statementsSlice';
+import { listenToAllSubStatements, listenToSubStatements } from '@/controllers/db/statements/listenToStatements';
+import { stat } from 'fs';
 
 interface Props {
 	parentStatement: Statement
@@ -12,11 +16,39 @@ interface Props {
 const SuggestionComment: FC<Props> = ({ statement, parentStatement }) => {
 	const { evaluationNumber } = useSuggestionComment({ parentStatement, statement });
 	const creator = statement.creator;
+	const comments = useSelector(statementSubsSelector(statement.statementId));
+
+	useEffect(() => {
+		const unsubscribe = listenToSubStatements(statement.statementId);
+
+		return () => unsubscribe();
+	}, [])
+
+	function handleCommentSubmit(ev: KeyboardEvent<HTMLTextAreaElement>): void {
+		if (ev.key === 'Enter' && !ev.shiftKey) {
+			ev.preventDefault();
+			const target = ev.target as HTMLTextAreaElement;
+			const text = target.value.trim();
+			if (text) {
+				// Add comment
+				console.log(statement.statementId)
+				saveStatementToDB({
+					text,
+					parentStatement: statement,
+					statementType: StatementType.statement
+				})
+			}
+			target.value = '';
+		}
+	}
 
 	return (
 		<div className={styles.suggestionComment}>
 			<p>{creator.displayName}</p>
-			<div>{statement.statement} <CreatorEvaluationIcon evaluationNumber={evaluationNumber} statement={statement} /></div>
+			<div>{statement.statement} <CreatorEvaluationIcon evaluationNumber={evaluationNumber} /></div>
+			{comments.map((comment) => (<p key={comment.statementId}>{comment.statement}</p>))}
+			<textarea name="commentInput" onKeyUp={handleCommentSubmit}></textarea>
+
 		</div>
 	)
 }
