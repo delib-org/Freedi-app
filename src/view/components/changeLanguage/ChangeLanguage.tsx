2fd2caa66c4cd1609c968d198b00b7152b34909b
Elizigi <elizigi876@gmail.com>
2025-03-16 20:22:38 +0200

    fixed broken menu and improved change language design

--- src/view/components/changeLanguage/ChangeLanguage.tsx
+++ src/view/components/changeLanguage/ChangeLanguage.tsx
@@ -5,6 +5,7 @@ import { LANGUAGES } from '@/constants/Languages';
 import { FC } from 'react';
 import Button, { ButtonType } from '../buttons/button/Button';
 import BackToMenuArrow from '@/assets/icons/backToMenuArrow.svg?react';
+import Close from '@/assets/icons/close.svg?react';
 
 interface ChangeLanguageProps {
 	background?: boolean;
@@ -42,63 +43,73 @@ const ChangeLanguage: FC<ChangeLanguageProps> = ({
 	}
 
 	return (
-		<div
-			className={`${styles.wrapper} ${background ? styles.background : ''}`}
-		>
+		<>
+			<div
+				className={`${styles.wrapper} ${background ? styles.background : ''}`}
+			>
+				{background && (
+					<div className={styles.XBtn}>
+						<Close onClick={() => setShowModal(false)}></Close>
+					</div>
+				)}
+				{background && (
+					<span>
+						<BackToMenuArrow
+							className={styles.backArrow}
+							onClick={goBackToMenu}
+						/>
+						<h1 className={styles.title}>
+							{t('Language selection')}
+						</h1>
+					</span>
+				)}
+				{background ? (
+					<div className={styles.optionsWrapper}>
+						{LANGUAGES.map(({ code, label, icon: Icon }) => (
+							<button
+								key={code}
+								className={`${styles.languageOption} ${currentLanguage === code ? styles.selected : ''}`}
+								onClick={
+									code === currentLanguage
+										? undefined
+										: () => handleLanguageChange(code)
+								}
+							>
+								<Icon className={styles.flag} />
+								<span>{t(label)}</span>
+							</button>
+						))}
+					</div>
+				) : (
+					<select
+						value={currentLanguage}
+						onChange={(e) => handleLanguageChange(e.target.value)}
+						className={styles.language}
+					>
+						{LANGUAGES.map(({ code, label }) => (
+							<option key={code} value={code}>
+								{label}
+							</option>
+						))}
+					</select>
+				)}
+
+				{background && (
+					<Button
+						text='close'
+						className={styles.closeBtn}
+						buttonType={ButtonType.SECONDARY}
+						onClick={() => setShowModal(false)}
+					></Button>
+				)}
+			</div>
 			{background && (
 				<button
+					className={styles.overlay}
 					onClick={() => setShowModal(false)}
-					className={styles.XBtn}
-				>
-					X
-				</button>
+				></button>
 			)}
-
-			{background && (
-				<span>
-					<BackToMenuArrow
-						className={styles.backArrow}
-						onClick={goBackToMenu}
-					/>
-					<h1 className={styles.title}>{t('Language selection')}</h1>
-				</span>
-			)}
-			{background ? (
-				<div className={styles.optionsWrapper}>
-					{LANGUAGES.map(({ code, label, icon: Icon }) => (
-						<button
-							key={code}
-							className={`${styles.languageOption} ${currentLanguage === code ? styles.selected : ''}`}
-							onClick={() => handleLanguageChange(code)}
-						>
-							<Icon className={styles.flag} />
-							<span>{t(label)}</span>
-						</button>
-					))}
-				</div>
-			) : (
-				<select
-					value={currentLanguage}
-					onChange={(e) => handleLanguageChange(e.target.value)}
-					className={styles.language}
-				>
-					{LANGUAGES.map(({ code, label }) => (
-						<option key={code} value={code}>
-							{label}
-						</option>
-					))}
-				</select>
-			)}
-
-			{background && (
-				<Button
-					text='close'
-					className={styles.closeBtn}
-					buttonType={ButtonType.PRIMARY}
-					onClick={() => setShowModal(false)}
-				></Button>
-			)}
-		</div>
+		</>
 	);
 };
 
