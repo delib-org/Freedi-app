efc9e43812fb9788f38cdcda203ad163bcdc1f31
Lee_D <leegreat21@gmail.com>
2025-03-20 16:35:18 +0200

    avatar-placeholder

--- src/view/components/menu/Menu.tsx
+++ src/view/components/menu/Menu.tsx
@@ -2,8 +2,11 @@ import { ComponentProps, FC, ReactNode } from 'react';
 import IconButton from '../iconButton/IconButton';
 import BurgerIcon from '@/assets/icons/burgerIcon.svg?react';
 import EllipsisIcon from '@/assets/icons/ellipsisIcon.svg?react';
-import './Menu.scss';
 import { useUserConfig } from '@/controllers/hooks/useUserConfig';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import './Menu.scss';
+import DefaultAvatar from '@/assets/images/avatar.jpg';
 
 interface MenuProps extends ComponentProps<'div'> {
 	iconColor: string;
@@ -24,6 +27,8 @@ const Menu: FC<MenuProps> = ({
 	footer,
 }) => {
 	const { dir } = useUserConfig();
+	const user = useSelector((state: RootState) => state.creator.creator);
+	const avatarSrc = user?.photoURL || DefaultAvatar;
 
 	if (!children) {
 		return null;
@@ -40,7 +45,18 @@ const Menu: FC<MenuProps> = ({
 			</IconButton>
 
 			<div className={`menu-content ${dir}${isCardMenu ? '--card-menu' : ''} ${isMenuOpen ? 'open' : ''}`}>
+				{/* HEADER STARTS HERE */}
+				<div className={`menu-header ${dir}`}>
+					<h2 className="menu-title">FreeDi</h2>
+					<div className="menu-user">
+						<img className="menu-avatar" src={avatarSrc} alt="User avatar" />
+						<span className="menu-username">{user?.displayName}</span>
+					</div>
+				</div>
+				{/* HEADER ENDS HERE */}
+
 				{children}
+
 				{footer && (
 					<div className="menu-footer" onClick={(e) => e.stopPropagation()}>
 						{footer}
