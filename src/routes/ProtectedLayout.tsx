7791602c5586faaef71f323d8bfa7db54ac8f699
Tal Yaron <tal.yaron@gmail.com>
2025-03-25 13:28:56 +0200

    fixed

--- src/routes/ProtectedLayout.tsx
+++ src/routes/ProtectedLayout.tsx
@@ -1,14 +1,29 @@
 import { AuthProvider } from '@/context/AuthContext';
+import { getStatementFromDB } from '@/controllers/db/statements/getStatement';
 import { useAuthorization } from '@/controllers/hooks/useAuthorization';
+import { setStatement, statementSelector } from '@/redux/statements/statementsSlice';
 import LoadingPage from '@/view/pages/loadingPage/LoadingPage';
 import Page401 from '@/view/pages/page401/Page401';
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useNavigate, useParams } from 'react-router';
 
 export default function ProtectedLayout() {
 	const { statementId } = useParams();
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const { loading, error, isAuthorized } = useAuthorization(statementId);
+	const statement = useSelector(statementSelector(statementId));
+
+	useEffect(() => {
+		if (!statement) {
+			getStatementFromDB(statementId).then((statement) => {
+				if (statement) {
+					dispatch(setStatement(statement));
+				}
+			});
+		}
+	}, [statement?.statementId]);
 
 	useEffect(() => {
 		if (!isAuthorized && !loading && !error) {
@@ -23,7 +38,7 @@ export default function ProtectedLayout() {
 
 	if (error) {
 
-		return <Page401 />; //TODO: create a page for this error	
+		return <Page401 />;
 	}
 
 	return (
