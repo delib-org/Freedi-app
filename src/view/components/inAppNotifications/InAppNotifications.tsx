3d9fa6525f70af7d9bea934b15e715383414d462
Olga Larina <21apr1988@gmail.com>
2025-04-02 15:12:46 +0300

    fixed styles

--- src/view/components/inAppNotifications/InAppNotifications.tsx
+++ src/view/components/inAppNotifications/InAppNotifications.tsx
@@ -3,34 +3,42 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router';
 import styles from './InAppNotifications.module.scss';
 import { useUserConfig } from '@/controllers/hooks/useUserConfig';
-import { store } from '@/redux/store';
 
 const InAppNotifications = () => {
 	const inAppNotifications = useSelector(inAppNotificationsSelector)
-	const { t, dir } = useUserConfig();
+	const { t } = useUserConfig();
 
 	return (
-		<div className={`${styles.inAppNotifications} ${styles[dir]}`}>
-			<span className={styles.notificationTitle}>{t('Notifications')}</span>
+		<div className={styles.inAppNotifications}>
+			{inAppNotifications && inAppNotifications.length > 0 ? (
+				<>
+					<span className={styles.notificationTitle}>{t('Notifications')}</span>
+					{inAppNotifications.map((notification) => {
 
-			{inAppNotifications.map((notification) => {
-
-				return (
-					<Link to={`/statement/${notification.parentId}`} key={notification.notificationId} className={styles.notificationLink}>
-						<div className={styles.notificationCard}>
-							<img
-								className={styles.avatar}
-								src={notification.creatorImage || '/src/assets/images/avatar.jpg'}
-								alt='User avatar'
-							/>
-							<div className={styles.text}>
-								<span className={styles.username}>{notification.creatorName}</span>
-								{notification.text}
-							</div>
-						</div>
-					</Link>
-				);
-			})}
+						return (
+							<Link
+								to={`/statement/${notification.parentId}`}
+								key={notification.notificationId}
+								className={styles.notificationLink}
+							>
+								<div className={styles.notificationCard}>
+									<img
+										className={styles.avatar}
+										src={notification.creatorImage || '/src/assets/images/avatar.jpg'}
+										alt="User avatar"
+									/>
+									<div className={styles.text}>
+										<span className={styles.username}>{notification.creatorName}</span>
+										{notification.text}
+									</div>
+								</div>
+							</Link>
+						);
+					})}
+				</>
+			) : (
+				<div className={styles.noNotifications}>{t('You have no new notifications')}</div>
+			)}
 		</div>
 	)
 }
