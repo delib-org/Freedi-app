d36e4324e75af9789480cddbe642ec3ab65e820c
Tal Yaron <tal.yaron@gmail.com>
2025-04-06 16:29:26 +0300

    init

--- src/view/components/footer/Footer.tsx
+++ src/view/components/footer/Footer.tsx
@@ -1,20 +1,26 @@
 import { FC } from "react";
-
 // icons
 import AddIcon from "@/assets/icons/plusIcon.svg?react";
-
-import "./Footer.scss";
+import styles from "./Footer.module.scss";
 import IconButton from "../iconButton/IconButton";
 
 interface Props {
-    onclick: () => void;
+	onclick: () => void;
 }
+
 const Footer: FC<Props> = ({ onclick }) => {
 	return (
-		<div className="footer" data-cy="add-statement">
-			<IconButton onClick={onclick} className="add-statement-button">
-				<AddIcon />
-			</IconButton>
+		<div className="wrapper">
+			<div className={styles.footer} data-cy="add-statement">
+
+				<button onClick={onclick} >
+					<AddIcon />
+				</button>
+				<button>
+					Groups
+				</button>
+				<button>Decisions</button>
+			</div>
 		</div>
 	);
 };
