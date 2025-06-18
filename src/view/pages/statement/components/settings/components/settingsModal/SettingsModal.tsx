916dc2539401649bc2ad484cdc908786b7c8df95
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2025-06-18 12:14:12 +0300

    add-header-to-modal

--- src/view/pages/statement/components/settings/components/settingsModal/SettingsModal.tsx
+++ src/view/pages/statement/components/settings/components/settingsModal/SettingsModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import CloseIcon from '@/assets/icons/close.svg?react';
+import ArrowLeftIcon from '@/assets/icons/arrow-left.svg?react';
 import styles from './SettingsModal.module.scss';
 
 interface Props {
@@ -10,11 +11,16 @@ interface Props {
 const SettingsModal: React.FC<Props> = ({ children, closeModal }) => {
 	return (
 		<div className={styles.modal}>
-
-			{children}
-			<div className="btns">
-				<button className='btn btn--primary' onClick={closeModal}>Close</button>
+			<div className={styles.modalHeader}>
+				<ArrowLeftIcon
+					className={styles.icon}
+				/>
+				<CloseIcon
+					className={styles.icon}
+					onClick={closeModal}			
+					/>
 			</div>
+			{children}
 		</div>
 	)
 }
