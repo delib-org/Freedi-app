dfb40d369e30af8329210f7a43b3fea76e504e26
Tal Yaron <tal.yaron@gmail.com>
2025-06-18 14:40:05 +0300

    ready

--- src/view/pages/statement/components/evaluations/components/evaluation/enhancedEvaluation/EnhancedEvaluation.tsx
+++ src/view/pages/statement/components/evaluations/components/evaluation/enhancedEvaluation/EnhancedEvaluation.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useRef, useEffect, useState } from 'react';
 import { getEvaluationThumbIdByScore } from '../../../statementsEvaluationCont';
 import styles from './EnhancedEvaluation.module.scss';
 import {
@@ -16,14 +16,19 @@ import { Tooltip } from '@/view/components/tooltip/Tooltip';
 
 interface EnhancedEvaluationProps {
 	statement: Statement;
-	shouldDisplayScore: boolean;
 }
+
+const indicatorWidth = 32; // Width of the bar in pixels, used for calculations
 
 const EnhancedEvaluation: FC<EnhancedEvaluationProps> = ({
 	statement,
-	shouldDisplayScore,
+
 }) => {
 	const { t, learning } = useUserConfig();
+	const [barWidth, setBarWidth] = useState<number>(0);
+	const evaluationBarRef = useRef<HTMLDivElement>(null);
+	const shouldDisplayScore = statement.statementSettings?.showEvaluation;
+
 	const evaluationScore = useAppSelector(
 		evaluationSelector(statement.statementId)
 	);
@@ -32,55 +37,73 @@ const EnhancedEvaluation: FC<EnhancedEvaluationProps> = ({
 		sumPro: 0,
 		sumCon: 0,
 		numberOfEvaluators: 0,
-	};
-	const avg = numberOfEvaluators !== 0 ? Math.round(((sumPro - sumCon) / numberOfEvaluators) * 100) / 100 : 0;
+	}; const avg = numberOfEvaluators !== 0 ? Math.round(((sumPro - sumCon) / numberOfEvaluators) * 100) / 100 : 0;
 	const consensus = Math.round(_consensus * 100) / 100;
 
+	useEffect(() => {
+		if (evaluationBarRef.current) {
+			const width = evaluationBarRef.current.offsetWidth;
+			setBarWidth(width);
+		}
+	}, []);
+
+	function barPosition(width: number, avg: number): number {
+		return (((avg + 1) / 2) * ((width - indicatorWidth) / width) * width);
+	}
+
+	function barColor(avg: number): string {
+		const colors = enhancedEvaluationsThumbs.map(
+			(thumb) => thumb.colorSelected
+		);
+		const index = Math.round(((avg + 1) / 2) * (colors.length - 1));
+
+		return colors[index] || colors[0];
+	}
+
+	console.log("learning.evaluation", learning.evaluation);
+
 	return (
-		<div className={styles['enhanced-evaluation']}>
-			<div className={styles['evaluation-thumbs']}>
-				{enhancedEvaluationsThumbs.map((evaluationThumb) => (
-					<EvaluationThumb
-						key={evaluationThumb.id}
-						evaluationThumb={evaluationThumb}
-						evaluationScore={evaluationScore}
-						statement={statement}
-					/>
-				))}
+		<div className={`${styles.evaluation}`}>
+			<div className={styles['enhanced-evaluation']}>
+				<div className={styles['evaluation-thumbs']}>
+					{enhancedEvaluationsThumbs.map((evaluationThumb) => (
+						<EvaluationThumb
+							key={evaluationThumb.id}
+							evaluationThumb={evaluationThumb}
+							evaluationScore={evaluationScore}
+							statement={statement}
+						/>
+					))}
+				</div>
+				{shouldDisplayScore && <Tooltip content={`${t("Average score")}: ${avg}`} position="top">
+					<div className={styles['evaluation-bar']} ref={evaluationBarRef}>
+						<div className={styles['evaluation-bar__indicator']} style={{ width: `${indicatorWidth}px`, right: `${barPosition(barWidth, avg)}px`, backgroundColor: barColor(avg) }}></div>
+					</div>
+				</Tooltip>}
+				{learning.evaluation > 0 && (
+					<div className={styles.explain}>
+						<div className={`${styles['evaluation-explain']}`}>
+							<span>{t('Disagree')}</span>
+							<span>{t('Agree')}</span>
+						</div>
+					</div>
+				)}
+
 			</div>
 			<div
 				className={`${styles['evaluation-score']} ${statement.consensus < 0 ? styles.negative : ''}`}
 			>
-				{shouldDisplayScore && (
-					<Tooltip content={t("Average score")} position="top">
-						<span className={styles.scoreValue}>{avg}</span>
-					</Tooltip>
-				)}
-
-				{shouldDisplayScore && (
-					<Tooltip content={t("Consensus score")} position="top">
-						<span className={styles.scoreValue}>{consensus}</span>
-					</Tooltip>
-				)}
-
 				{shouldDisplayScore &&
 					numberOfEvaluators &&
 					numberOfEvaluators > 0 ? (
-					<Tooltip content={t("Number of evaluators")} position="bottom">
+					<Tooltip content={`${t("Number of evaluators")}. ${t("Consensus")}: ${consensus}`} position="bottom">
 						<span className={styles['total-evaluators']}>
 							{' '}({numberOfEvaluators})
 						</span>
 					</Tooltip>
 				) : null}
 			</div>
-			{learning.evaluation > 0 && (
-				<div className={styles.explain}>
-					<div className={`${styles['evaluation-explain']}`}>
-						<span>{t('Disagree')}</span>
-						<span>{t('Agree')}</span>
-					</div>
-				</div>
-			)}
+
 			<div />
 		</div>
 	);
