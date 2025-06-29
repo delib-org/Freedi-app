89136430a98470dba8c88548af09b01b6b90c9eb
Tal Yaron <tal.yaron@gmail.com>
2025-06-29 09:30:43 +0300

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.freedi.app',
+  appName: 'freedi-app',
+  webDir: 'dist'
+};
+
+export default config;
