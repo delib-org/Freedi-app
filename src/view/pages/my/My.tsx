cd48aeacb1fcc5b107947dfb800b01d0526b07ce
Elizigi <elizigi876@gmail.com>
2025-07-15 18:36:57 +0300

    added radio buttons

--- src/view/pages/my/My.tsx
+++ src/view/pages/my/My.tsx
@@ -1,25 +1,24 @@
 import { useUserConfig } from '@/controllers/hooks/useUserConfig';
 import { creatorSelector } from '@/redux/creator/creatorSlice';
 
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import GeneralHeader from '@/view/components/generalHeader/GeneralHeader';
-import Checkbox from '@/view/components/checkbox/Checkbox';
 import { setUserAdvanceUserToDB } from '@/controllers/db/user/setUser';
 
-import './my.scss'
+import './my.scss';
 import profilePicPH from '@/assets/images/user-page.png';
 import React, { useRef, useState } from 'react';
+import RadioButtonWithLabel from '@/view/components/radioButtonWithLabel/RadioButtonWithLabel';
+import Button from '@/view/components/buttons/button/Button';
+import { useNavigate } from 'react-router';
 
 const My = () => {
 	const user = useSelector(creatorSelector);
 	const { t } = useUserConfig();
-
+	const isAdvancedUser = user?.advanceUser;
 	const [selectedImage, setSelectedImage] = useState<string | null>(null);
 	const fileInputRef = useRef<HTMLInputElement | null>(null);
-
-	function handleSetAdvanceUser() {
-		setUserAdvanceUserToDB(!user?.advanceUser);
-	}
+	const navigate = useNavigate();
 
 	function handleImageClick() {
 		fileInputRef.current?.click();
@@ -40,34 +39,50 @@ const My = () => {
 		<div className='page'>
 			<GeneralHeader />
 
-			<div className="myContainer">
-				<h1>{t("Hello")} {user?.displayName}</h1>
+			<div className='myContainer'>
+				<h1>
+					{t('Hello')} {user?.displayName}
+				</h1>
 
 				{/* Clickable image area */}
 				<img
 					src={selectedImage || user?.photoURL || profilePicPH}
-					alt="Profile"
+					alt='Profile'
 					className='profilePicPH'
 					onClick={handleImageClick}
 				/>
 				<input
-					type="file"
-					accept="image/*"
+					type='file'
+					accept='image/*'
 					ref={fileInputRef}
 					style={{ display: 'none' }}
 					onChange={handleImageChange}
 				/>
 				<p className='profilePicTitle'>Change profile picture</p>
+				<div className='radioContainer'>
+					<h3>Profile Setting</h3>
 
-				<Checkbox
-					label={t("Advance User")}
-					isChecked={user?.advanceUser}
-					onChange={handleSetAdvanceUser}
-				/>
-
+					<RadioButtonWithLabel
+						id='simple-user'
+						labelText={t('Simple User')}
+						checked={!isAdvancedUser}
+						onChange={() => setUserAdvanceUserToDB(false)}
+					/>
+					<RadioButtonWithLabel
+						id='advanced-user'
+						labelText={t('Advance User')}
+						checked={isAdvancedUser}
+						onChange={() => setUserAdvanceUserToDB(true)}
+					/>
+					<Button
+						className='save'
+						text='save'
+						onClick={() => navigate('/')}
+					/>
+				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default My
+export default My;
