e8df182ece72e974bee0443c6789e8ab28e37393
Elizigi <elizigi876@gmail.com>
2025-07-22 01:39:48 +0300

    updated main survey style when creating a new survey

--- src/view/pages/statement/components/settings/components/settingsModal/SettingsModal.tsx
+++ src/view/pages/statement/components/settings/components/settingsModal/SettingsModal.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 import styles from './SettingsModal.module.scss';
 
 interface Props {
 	children?: React.ReactNode;
 	closeModal?: () => void;
+	isFullScreen?: boolean;
 }
 
-const SettingsModal: React.FC<Props> = ({ children, closeModal }) => {
+const SettingsModal: React.FC<Props> = ({
+	children,
+	closeModal,
+	isFullScreen = false,
+}) => {
 	return (
-		<div className={styles.modal}>
-
+		<div className={isFullScreen ? styles.fullScreenModal : styles.modal}>
 			{children}
-			<div className="btns">
-				<button className='btn btn--primary' onClick={closeModal}>Close</button>
+			<div className='btns'>
+				<button className='btn btn--primary' onClick={closeModal}>
+					Close
+				</button>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default SettingsModal
+export default SettingsModal;
