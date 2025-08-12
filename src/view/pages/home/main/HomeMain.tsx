b5628c4520c2239660221cf65f4892e8c6b72970
Elizigi <elizigi876@gmail.com>
2025-08-12 19:39:13 +0300

    fixed

--- src/view/pages/home/main/HomeMain.tsx
+++ src/view/pages/home/main/HomeMain.tsx
@@ -6,7 +6,10 @@ import styles from './HomeMain.module.scss';
 import MainCard from './mainCard/MainCard';
 import bike from '@/assets/images/bike.png';
 import { useAppSelector } from '@/controllers/hooks/reduxHooks';
-import { statementsSubscriptionsSelector, topSubscriptionsSelector } from '@/redux/statements/statementsSlice';
+import {
+	statementsSubscriptionsSelector,
+	topSubscriptionsSelector,
+} from '@/redux/statements/statementsSlice';
 
 // Custom components
 import Footer from '@/view/components/footer/Footer';
@@ -23,19 +26,25 @@ const HomeMain = () => {
 	// Hooks
 	const showNewStatementModal = useAppSelector(selectNewStatementShowModal);
 	const [loading, setLoading] = useState(true);
-	const [subPage, setSubPage] = useState<"decisions" | "groups">("groups");
-	const [subPageTitle, setSubPageTitle] = useState<"Decisions" | "Groups">("Decisions");
+	const [subPage, setSubPage] = useState<'decisions' | 'groups'>('groups');
+	const [subPageTitle, setSubPageTitle] = useState<'Decisions' | 'Groups'>(
+		'Decisions'
+	);
 	const user = useSelector(creatorSelector);
 	const { t } = useUserConfig();
-	const userId = user?.uid || "";
+	const userId = user?.uid || '';
 
 	const topSubscriptions = useAppSelector(topSubscriptionsSelector)
-		.sort((a, b) => b.lastUpdate - a.lastUpdate)
-		.filter((sub) => sub.user?.uid === user?.uid && sub.statement.statementType === StatementType.group);
-
+		.sort((a, b) => b.statement.createdAt - a.statement.createdAt)
+		.filter(
+			(sub) =>
+				sub.user?.uid === user?.uid &&
+				sub.statement.statementType === StatementType.group
+		);
+	
 	const latestDecisions = useAppSelector(statementsSubscriptionsSelector)
 		.filter((sub) => sub.statement.statementType === StatementType.question)
-		.sort((a, b) => b.lastUpdate - a.lastUpdate)
+		.sort((a, b) => b.statement.createdAt - a.statement.createdAt);
 
 	useEffect(() => {
 		setTimeout(() => {
@@ -49,16 +58,15 @@ const HomeMain = () => {
 
 	useEffect(() => {
 		if (userId && user.advanceUser) {
-			setSubPage("groups");
+			setSubPage('groups');
+		} else {
+			setSubPage('decisions');
 		}
-		else {
-			setSubPage("decisions");
-		}
-	}, [userId])
+	}, [userId]);
 
 	useEffect(() => {
-		setSubPageTitle(subPage === "decisions" ? "Decisions" : "Groups");
-	}, [subPage])
+		setSubPageTitle(subPage === 'decisions' ? 'Decisions' : 'Groups');
+	}, [subPage]);
 
 	return (
 		<main className='home-page__main slide-in'>
@@ -76,9 +84,11 @@ const HomeMain = () => {
 						topSubscriptions.length > 0 ? 'start' : 'center',
 				}}
 			>
-				{showNewStatementModal && <div className={styles.addStatementModal}>
-					<NewStatement />
-				</div>}
+				{showNewStatementModal && (
+					<div className={styles.addStatementModal}>
+						<NewStatement />
+					</div>
+				)}
 				<h2>{t(subPageTitle)}</h2>
 				{(() => {
 					if (loading) {
@@ -89,19 +99,24 @@ const HomeMain = () => {
 						);
 					}
 
-					const itemsToRender = subPage === "groups" ? topSubscriptions : latestDecisions;
+					const itemsToRender =
+						subPage === 'groups'
+							? topSubscriptions
+							: latestDecisions;
 
-					return itemsToRender.map((sub) => (
-						subPage === "groups" ?
+					return itemsToRender.map((sub) =>
+						subPage === 'groups' ? (
 							<MainCard
 								key={sub.statementId}
 								subscription={sub}
-							/> :
+							/>
+						) : (
 							<MainQuestionCard
 								key={sub.statementId}
 								simpleStatement={sub.statement}
 							/>
-					));
+						)
+					);
 				})()}
 			</div>
 			<Footer setSubPage={setSubPage} subPage={subPage} />
