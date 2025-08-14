a7fcfb4539ac23583d1d9986707954e1704d5c7c
Elizigi <elizigi876@gmail.com>
2025-08-14 22:45:49 +0300

    improvedFunctionality

--- src/view/pages/statement/components/subQuestionsMap/SubQuestionsMap.tsx
+++ src/view/pages/statement/components/subQuestionsMap/SubQuestionsMap.tsx
@@ -1,90 +1,92 @@
-import { Results, Statement, StatementType } from 'delib-npm';
-import styles from './SubQuestionsMap.module.scss';
-import SubQuestionNode from './subQuestionNode/SubQuestionNode';
-import { useMindMap } from '../map/MindMapMV';
-import { type JSX } from 'react';
-import { useParams } from 'react-router';
+import { Results, Statement, StatementType } from "delib-npm";
+import styles from "./SubQuestionsMap.module.scss";
+import SubQuestionNode from "./subQuestionNode/SubQuestionNode";
+import { useMindMap } from "../map/MindMapMV";
+import { type JSX } from "react";
+import { useParams } from "react-router";
 
 interface SubQuestionsMapProps {
-	readonly statement: Statement;
+  readonly statement: Statement;
 }
 
 const SubQuestionsMap = ({ statement }: SubQuestionsMapProps) => {
-	const { results } = useMindMap(statement.topParentId);
-	const { screen } = useParams();
+  const { results } = useMindMap(statement.topParentId);
+  const { screen } = useParams();
 
-	if (
-		screen === 'mind-map' ||
-		screen === 'polarization-index' ||
-		screen === 'agreement-map'
-	)
-		return null;
+  if (
+    screen === "mind-map" ||
+    screen === "polarization-index" ||
+    screen === "agreement-map"
+  )
+    return null;
 
-	if (!results) return null;
-	const defaultDepth = 1;
-	const filterResults = (results: Results): Results => {
-		return {
-			top: results.top,
-			sub: results.sub
-				.map(filterResults)
-				.filter(
-					(res) =>
-						res.top.statementType !== StatementType.option ||
-						res.sub.length > 0
-				),
-		};
-	};
-	const filteredResults = filterResults(results);
+  if (!results) return null;
+  const defaultDepth = 1;
+  const filterResults = (results: Results): Results => {
+    return {
+      top: results.top,
+      sub: results.sub
+        .map(filterResults)
+        .filter(
+          (res) =>
+            res.top.statementType !== StatementType.option || res.sub.length > 0
+        ),
+    };
+  };
+  const filteredResults = filterResults(results);
 
-	const parseTree = (
-		tResults: Results,
-		currentDepth: number
-	): JSX.Element[] => {
-		currentDepth++;
+  const parseTree = (
+    tResults: Results,
+    currentDepth: number
+  ): JSX.Element[] => {
+    currentDepth++;
 
-		return tResults.sub.map((res, index) => (
-			<div key={res.top.statement + index}>
-				<SubQuestionNode
-					statement={res.top}
-					depth={currentDepth}
-					last={index === tResults.sub.length - 1}
-					hasChildren={res.sub.length !== 0}
-					height={calculateHeight(res)}
-				/>
-				{parseTree(filterResults(res), currentDepth)}
-			</div>
-		));
-	};
+    return tResults.sub.map((res, index) => (
+      <div key={res.top.statement + index}>
+        <SubQuestionNode
+          statement={res.top}
+          depth={currentDepth}
+          hasChildren={res.sub.length !== 0}
+          childAmount={res.sub.length}
+          height={calculateHeight(res, 0, true)}
+        />
+        {parseTree(filterResults(res), currentDepth)}
+      </div>
+    ));
+  };
 
-	const calculateHeight = (
-		cResults: Results,
-		height = 0,
-		firstStatement = false
-	) => {
-		const number = firstStatement ? 0 : 1;
-		cResults.sub.forEach((res, index) => {
-			height++;
-			if (index === cResults.sub.length - number) return height;
+  const calculateHeight = (cResults, height = 0, root = false) => {
+    if (root) {
+      for (let i = 0; i < cResults.sub.length - 1; i++) {
+        height += 1;
+        height = calculateHeight(cResults.sub[i], height, false);
+      }
+    } else {
+      height += 1;
+      if (cResults.sub && cResults.sub.length > 0) {
+        cResults.sub.forEach((child) => {
+          height = calculateHeight(child, height, false);
+        });
+      }
+    }
 
-			height = calculateHeight(res, height, firstStatement);
-		});
+    return root ? height - 1 : height;
+  };
 
-		return height;
-	};
-
-	return (
-		<div className={styles.subQuestionsMapContainer}>
-			<div className={styles.title}>
-				<h3>Statement Map</h3>
-			</div>
-			<SubQuestionNode
-				statement={results.top}
-				depth={defaultDepth}
-				hasChildren={results.sub.length > 0}
-				height={calculateHeight(filteredResults, 0, true)}
-			/>
-			{parseTree(filteredResults, defaultDepth)}
-		</div>
-	);
+  return (
+    <div className={styles.subQuestionsMapContainer}>
+      <div className={styles.title}>
+        <h3>Statement Map</h3>
+      </div>
+      <SubQuestionNode
+        statement={results.top}
+        depth={defaultDepth}
+        childAmount={results.sub.length}
+        hasChildren={results.sub.length > 0}
+        height={calculateHeight(filteredResults, 0, true)}
+      />
+      {parseTree(filteredResults, defaultDepth)}
+    </div>
+  );
 };
 export default SubQuestionsMap;
--- src/view/pages/statement/components/subQuestionsMap/subQuestionNode/SubQuestionNode.tsx
+++ src/view/pages/statement/components/subQuestionsMap/subQuestionNode/SubQuestionNode.tsx
@@ -1,85 +1,82 @@
-import { useNavigate, useParams } from 'react-router';
-import styles from './subQuestionNode.module.scss';
-import ArrowLeft from '@/assets/icons/backToMenuArrow.svg?react';
-import { FC, useState } from 'react';
-import { Statement } from 'delib-npm';
+import { useNavigate, useParams } from "react-router";
+import styles from "./subQuestionNode.module.scss";
+import ArrowLeft from "@/assets/icons/backToMenuArrow.svg?react";
+import { FC, useState } from "react";
+import { Statement } from "delib-npm";
 
 interface SubQuestionNodeProps {
-	statement: Statement;
-	depth: number;
-	last?: boolean;
-	hasChildren?: boolean;
-	height?: number;
+  statement: Statement;
+  depth: number;
+  childAmount: number;
+  hasChildren?: boolean;
+  height?: number;
 }
 
 const SubQuestionNode: FC<SubQuestionNodeProps> = ({
-	statement,
-	depth = -1,
-	height = 0,
-	last = false,
-	hasChildren = false,
+  statement,
+  childAmount,
+  depth = -1,
+  height = 0,
+  hasChildren = false,
 }) => {
-	const topStatement = depth <= 1;
-	const navigate = useNavigate();
-	const { statementId } = useParams();
-	const [clicked, setClicked] = useState(false);
-	const handleClick = () => {
-		setClicked(true);
-		setTimeout(() => {
-			setClicked(false);
-			navigate(`/statement/${statement.statementId}/chat`, {
-				state: { from: window.location.pathname },
-			});
-		}, 302);
-	};
-	const isInStatement = statement.statementId === statementId;
-	const styleMargin = 4.6;
-	const marginLeft = `${depth}rem`;
-	const styleGraph = () => {
-		const classNames = [styles.borderDefault];
+  const topStatement = depth <= 1;
+  const navigate = useNavigate();
+  const { statementId } = useParams();
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+      navigate(`/statement/${statement.statementId}/chat`, {
+        state: { from: window.location.pathname },
+      });
+    }, 302);
+  };
+  const isInStatement = statement.statementId === statementId;
+  const styleMargin = 4.6;
+  const marginLeft = `${depth}rem`;
+  const styleGraph = () => {
+    const classNames = [styles.borderDefault];
+    if (hasChildren) classNames.push(styles.borderRight);
 
-		
-		if (hasChildren && height < 1) classNames.push(styles.borderBottom);
-		classNames.push(styles.borderTop);
+    if (hasChildren) classNames.push(styles.borderBottom);
 
-		return classNames.join(' ');
-	};
+    return classNames.join(" ");
+  };
 
-	return (
-		<div className={styles.SubQuestionNodeContainer}>
-			<div
-				className={`${styles.node} ${isInStatement ? styles.green : ''} ${depth <= 1 && !isInStatement ? styles.group : ''}`}
-			>
-				<h3>{statement.statement}</h3>
-				{!isInStatement && (
-					<button
-						className={clicked ? styles.animate : ''}
-						onClick={handleClick}
-					>
-						<ArrowLeft></ArrowLeft>
-					</button>
-				)}
-			</div>
+  return (
+    <div className={styles.SubQuestionNodeContainer}>
+      <div
+        className={`${styles.node} ${isInStatement ? styles.green : ""} ${depth <= 1 && !isInStatement ? styles.group : ""}`}
+      >
+        <h3>{statement.statement}</h3>
+        {!isInStatement && (
+          <button
+            className={clicked ? styles.animate : ""}
+            onClick={handleClick}
+          >
+            <ArrowLeft></ArrowLeft>
+          </button>
+        )}
+      </div>
 
-			{
-				<div
-					className={styleGraph()}
-					style={{ marginLeft: marginLeft }}
-				>
-					{(topStatement || height > 0) && (
-						<div
-							className={styles.borderRightTop}
-							style={{
-								marginLeft: `${depth}rem`,
-								height: `${height * styleMargin}rem`,
-							}}
-						></div>
-					)}
-					<div className={styles.blueDot}>●</div>
-				</div>
-			}
-		</div>
-	);
+      {
+        <div className={styleGraph()} style={{ marginLeft: marginLeft }}>
+          {(topStatement || height > 0) && childAmount > 1 && (
+            <div
+              className={styles.borderRightTop}
+              style={{
+                marginLeft: `${depth}rem`,
+                height: `${height * styleMargin}rem`,
+                top: `${hasChildren ? styleMargin : 0}rem`,
+              }}
+            ></div>
+          )}
+          <div className={styles.blueDot}>●</div>
+        </div>
+      }
+    </div>
+  );
 };
 
 export default SubQuestionNode;
