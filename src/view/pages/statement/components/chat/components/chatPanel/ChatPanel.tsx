9df37cb4b2af9699b9ffafe1e347fa841bdd711e
Elizigi <elizigi876@gmail.com>
2025-08-17 23:01:29 +0300

    update

--- src/view/pages/statement/components/chat/components/chatPanel/ChatPanel.tsx
+++ src/view/pages/statement/components/chat/components/chatPanel/ChatPanel.tsx
@@ -1,30 +1,49 @@
-import { useParams } from 'react-router';
-import styles from './ChatPanel.module.scss';
-import Chat from '../../Chat';
-import ChatIcon from '@/assets/icons/roundedChatDotIcon.svg?react';
+import { useParams } from "react-router";
+import styles from "./ChatPanel.module.scss";
+import Chat from "../../Chat";
+import ChatIcon from "@/assets/icons/roundedChatDotIcon.svg?react";
+import { useState } from "react";
+import ChevronLeftIcon from "@/assets/icons/chevronLeftIcon.svg?react";
 
 const ChatPanel = () => {
-	const { screen } = useParams();
+  const { screen } = useParams();
+  const [isSideChatOpen, setIsSideChatOpen] = useState(false);
+  if (
+    screen === "mind-map" ||
+    screen === "polarization-index" ||
+    screen === "agreement-map" ||
+    screen === "chat"
+  )
+    return null;
 
-	if (
-		screen === 'mind-map' ||
-		screen === 'polarization-index' ||
-		screen === 'agreement-map'
-	)
-		return null;
-
-	return (
-		<div className={styles.chatPanelContainer}>
-			<div className={styles.sideChatTitle}>
-				<ChatIcon></ChatIcon> <h5>Free Discussion</h5>
-			</div>
-			<p>
-				Questions and topics that emerged from the main discussion
-				thread
-			</p>
-			<Chat sideChat={true} />
-		</div>
-	);
+  return (
+    <>
+      {isSideChatOpen ? (
+        <div className={styles.chatPanelContainer}>
+          <div className={styles.sideChatTitle}>
+            <ChatIcon /> <h5>Free Discussion</h5>
+          </div>
+		  <button onClick={()=>setIsSideChatOpen(false)} className={styles.closeChatPanel}>  <ChevronLeftIcon /></button>
+          <p>
+            Questions and topics that emerged from the main discussion thread
+          </p>
+          <Chat sideChat={true} />
+        </div>
+      ) : (
+        <div className={styles.chatPanelContainerClosed}>
+          <button
+            onClick={() => setIsSideChatOpen(true)}
+            className={styles.openChatPanelBtn}
+          >
+            <ChevronLeftIcon />
+          </button>
+          <button className={styles.openChatPanelBtn}>
+            <ChatIcon />
+          </button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ChatPanel;
