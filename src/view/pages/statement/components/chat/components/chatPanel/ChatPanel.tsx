fae7b628dab8cbec602d4174606f41114de98e00
Elizigi <elizigi876@gmail.com>
2025-08-17 23:58:23 +0300

    desktopVersion

--- src/view/pages/statement/components/chat/components/chatPanel/ChatPanel.tsx
+++ src/view/pages/statement/components/chat/components/chatPanel/ChatPanel.tsx
@@ -2,12 +2,30 @@ import { useParams } from "react-router";
 import styles from "./ChatPanel.module.scss";
 import Chat from "../../Chat";
 import ChatIcon from "@/assets/icons/roundedChatDotIcon.svg?react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ChevronLeftIcon from "@/assets/icons/chevronLeftIcon.svg?react";
+import { useSelector } from "react-redux";
+import { inAppNotificationsSelector } from "@/redux/notificationsSlice/notificationsSlice";
+import { NotificationType } from "delib-npm";
+import { creatorSelector } from "@/redux/creator/creatorSlice";
 
 const ChatPanel = () => {
-  const { screen } = useParams();
+  const { screen, statementId } = useParams();
   const [isSideChatOpen, setIsSideChatOpen] = useState(false);
+  const [isAnimatingOut, setIsAnimatingOut] = useState(false);
+  const chatPanelRef = useRef(null);
+  useEffect(() => {
+    if (isSideChatOpen && chatPanelRef.current) {
+      chatPanelRef.current.scrollTop = 0;
+    }
+  }, [isSideChatOpen]);
+
+  const creator = useSelector(creatorSelector);
+
+  const inAppNotificationsList: NotificationType[] = useSelector(
+    inAppNotificationsSelector
+  ).filter((n) => n.creatorId !== creator?.uid && n.parentId === statementId);
+
   if (
     screen === "mind-map" ||
     screen === "polarization-index" ||
@@ -16,14 +34,43 @@ const ChatPanel = () => {
   )
     return null;
 
+  const closeChatPanel = () => {
+        setIsAnimatingOut(true); 
+        setTimeout(() => {
+            setIsSideChatOpen(false);
+            setIsAnimatingOut(false);
+        }, 502); 
+    };
+
+  const content = (
+    <div className={styles.icon}>
+      {inAppNotificationsList.length > 0 && (
+        <div className={styles.redCircle}>
+          {inAppNotificationsList.length < 10
+            ? inAppNotificationsList.length
+            : `9+`}
+        </div>
+      )}
+      <ChatIcon />
+    </div>
+  );
+
   return (
     <>
       {isSideChatOpen ? (
-        <div className={styles.chatPanelContainer}>
+        <div
+          className={`${styles.chatPanelContainer} ${isAnimatingOut ? styles.chatPanelClosing : ""}`}
+          ref={chatPanelRef}
+        >
           <div className={styles.sideChatTitle}>
             <ChatIcon /> <h5>Free Discussion</h5>
           </div>
-		  <button onClick={()=>setIsSideChatOpen(false)} className={styles.closeChatPanel}>  <ChevronLeftIcon /></button>
+          <button
+            onClick={() => closeChatPanel()}
+            className={styles.closeChatPanel}
+          >
+            <ChevronLeftIcon />
+          </button>
           <p>
             Questions and topics that emerged from the main discussion thread
           </p>
@@ -33,13 +80,11 @@ const ChatPanel = () => {
         <div className={styles.chatPanelContainerClosed}>
           <button
             onClick={() => setIsSideChatOpen(true)}
-            className={styles.openChatPanelBtn}
+            className={`${styles.openChatPanelBtn} ${styles.toTheRight}`}
           >
             <ChevronLeftIcon />
           </button>
-          <button className={styles.openChatPanelBtn}>
-            <ChatIcon />
-          </button>
+          <button className={styles.openChatPanelBtn}>{content} </button>
         </div>
       )}
     </>
