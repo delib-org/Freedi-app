174b0ceb478d21e8cc1e16a5f29d3839bef1cf66
Tal Yaron <tal.yaron@gmail.com>
2025-10-01 07:32:07 +0300

    remove lazyloading

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,35 +1,28 @@
-import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router';
 import App from '@/App';
 import Start from '@/view/pages/start/Start';
 import ErrorPage from '@/view/pages/error/ErrorPage';
 import ProtectedLayout from './ProtectedLayout';
-import RouteLoader from '@/view/components/loading/RouteLoader';
-
-// Wrap lazy components with Suspense
-const withSuspense = (Component: React.ComponentType) => {
-	return (
-		<Suspense fallback={<RouteLoader />}>
-			<Component />
-		</Suspense>
-	);
-};
 
 // Error pages
+import Page401 from '@/view/pages/page401/Page401';
+import Page404 from '@/view/pages/page404/Page404';
 
-// Mass Consensus pages - Lazy load these as they're used in specific flows
-const MassConsensus = lazy(() => import('@/view/pages/massConsensus/MassConsensus'));
-const Introduction = lazy(() => import('@/view/pages/massConsensus/introduction/Introduction'));
-const MassConsensusQuestion = lazy(() => import('@/view/pages/massConsensus/massConsesusQuestion/MassConsesusQuestion'));
-const RandomSuggestions = lazy(() => import('@/view/pages/massConsensus/randomSuggestions/RandomSuggestions'));
-const TopSuggestions = lazy(() => import('@/view/pages/massConsensus/topSuggestions/TopSuggestions'));
-const VotingSuggestions = lazy(() => import('@/view/pages/massConsensus/votingSuggestions/VotingSuggestions'));
-const ResultsSummary = lazy(() => import('@/view/pages/massConsensus/resultsSummary/ResultsSummary'));
-const LeaveFeedback = lazy(() => import('@/view/pages/massConsensus/leaveFeedback/LeaveFeedback'));
-const MassConsensusMySuggestions = lazy(() => import('@/view/pages/massConsensus/mySuggestions/MassConsensusMySuggestions'));
+// Mass Consensus pages - Direct imports for instant navigation
+import MassConsensus from '@/view/pages/massConsensus/MassConsensus';
+import Introduction from '@/view/pages/massConsensus/introduction/Introduction';
+import MassConsensusQuestion from '@/view/pages/massConsensus/massConsesusQuestion/MassConsesusQuestion';
+import RandomSuggestions from '@/view/pages/massConsensus/randomSuggestions/RandomSuggestions';
+import TopSuggestions from '@/view/pages/massConsensus/topSuggestions/TopSuggestions';
+import VotingSuggestions from '@/view/pages/massConsensus/votingSuggestions/VotingSuggestions';
+import ResultsSummary from '@/view/pages/massConsensus/resultsSummary/ResultsSummary';
+import LeaveFeedback from '@/view/pages/massConsensus/leaveFeedback/LeaveFeedback';
+import MassConsensusMySuggestions from '@/view/pages/massConsensus/mySuggestions/MassConsensusMySuggestions';
+import ThankYou from '@/view/pages/massConsensus/thankYou/ThankYou';
+import UserDemographicMC from '@/view/pages/massConsensus/massConsesusQuestion/userDemographicMC/UserDemographicMC';
 
-// Protected routes - Heavy component, definitely lazy load
-const StatementMain = lazy(() => import('@/view/pages/statement/StatementMain'));
+// Protected routes
+import StatementMain from '@/view/pages/statement/StatementMain';
 
 // Public routes
 import Home from '@/view/pages/home/Home';
@@ -38,16 +31,13 @@ import AddStatement from '@/view/pages/home/main/addStatement/AddStatement';
 import LoginPage from '@/view/pages/login/LoginFirst';
 import MemberRejection from '@/view/pages/memberRejection/MemberRejection';
 
-// Mass Consensus URLs
+// User pages
+import My from '@/view/pages/my/My';
+import CheckNotifications from '@/view/pages/settings/ChecNotifications';
+import MySuggestions from '@/view/pages/my-suggestions/MySuggestions';
 
-import Page401 from '@/view/pages/page401/Page401';
-import Page404 from '@/view/pages/page404/Page404';
+// Mass Consensus URLs
 import { MassConsensusPageUrls } from 'delib-npm';
-const ThankYou = lazy(() => import('@/view/pages/massConsensus/thankYou/ThankYou'));
-const UserDemographicMC = lazy(() => import('@/view/pages/massConsensus/massConsesusQuestion/userDemographicMC/UserDemographicMC'));
-const My = lazy(() => import('@/view/pages/my/My'));
-const CheckNotifications = lazy(() => import('@/view/pages/settings/ChecNotifications'));
-const MySuggestions = lazy(() => import('@/view/pages/my-suggestions/MySuggestions'));
 
 // Define errorRoutes
 const errorRoutes = [
@@ -69,51 +59,51 @@ const errorRoutes = [
 const massConsensusRoutes = [
 	{
 		path: 'mass-consensus/:statementId',
-		element: withSuspense(MassConsensus),
+		element: <MassConsensus />,
 		children: [
 			{
 				index: true,
-				element: withSuspense(Introduction),
+				element: <Introduction />,
 			},
 			{
 				path: MassConsensusPageUrls.introduction,
-				element: withSuspense(Introduction),
+				element: <Introduction />,
 			},
 			{
 				path: MassConsensusPageUrls.userDemographics,
-				element: withSuspense(UserDemographicMC),
+				element: <UserDemographicMC />,
 			},
 			{
 				path: MassConsensusPageUrls.question,
-				element: withSuspense(MassConsensusQuestion),
+				element: <MassConsensusQuestion />,
 			},
 			{
 				path: MassConsensusPageUrls.randomSuggestions,
-				element: withSuspense(RandomSuggestions),
+				element: <RandomSuggestions />,
 			},
 			{
 				path: MassConsensusPageUrls.topSuggestions,
-				element: withSuspense(TopSuggestions),
+				element: <TopSuggestions />,
 			},
 			{
 				path: MassConsensusPageUrls.voting,
-				element: withSuspense(VotingSuggestions),
+				element: <VotingSuggestions />,
 			},
 			{
 				path: MassConsensusPageUrls.results,
-				element: withSuspense(ResultsSummary),
+				element: <ResultsSummary />,
 			},
 			{
 				path: MassConsensusPageUrls.leaveFeedback,
-				element: withSuspense(LeaveFeedback),
+				element: <LeaveFeedback />,
 			},
 			{
 				path: MassConsensusPageUrls.thankYou,
-				element: withSuspense(ThankYou),
+				element: <ThankYou />,
 			},
 			{
 				path: 'my-suggestions',
-				element: withSuspense(MassConsensusMySuggestions),
+				element: <MassConsensusMySuggestions />,
 			},
 		],
 	},
@@ -123,35 +113,35 @@ const massConsensusRoutes = [
 const protectedRoutes = [
 	{
 		path: 'stage/:statementId',
-		element: withSuspense(StatementMain),
+		element: <StatementMain />,
 	},
 	{
 		path: 'statement/:statementId',
-		element: withSuspense(StatementMain),
+		element: <StatementMain />,
 		children: [
 			{
 				path: ':sort',
-				element: withSuspense(StatementMain),
+				element: <StatementMain />,
 			},
 		],
 	},
 	{
 		path: 'statement-screen/:statementId',
-		element: withSuspense(StatementMain),
+		element: <StatementMain />,
 		children: [
 			{
 				path: ':screen',
-				element: withSuspense(StatementMain),
+				element: <StatementMain />,
 			},
 		],
 	},
 	{
 		path: 'stage/:statementId/:sort',
-		element: withSuspense(StatementMain),
+		element: <StatementMain />,
 	},
 	{
 		path: 'my-suggestions/statement/:statementId',
-		element: withSuspense(MySuggestions),
+		element: <MySuggestions />,
 	},
 	// ... other protected routes
 ];
@@ -210,11 +200,11 @@ export const router = createBrowserRouter([
 			// User profile routes that need authentication but not statement authorization
 			{
 				path: 'my',
-				element: withSuspense(My),
+				element: <My />,
 			},
 			{
 				path: 'my/check-notifications',
-				element: withSuspense(CheckNotifications),
+				element: <CheckNotifications />,
 			},
 		],
 	},
