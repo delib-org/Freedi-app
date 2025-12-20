dbdaff8b1c3f003a2554e76d934ccf931b6d270a
Tal Yaron <tal.yaron@gmail.com>
2025-12-20 22:05:30 +0200

    summrize -turnicated

---
+++ functions/src/config/cors.ts
@@ -0,0 +1,20 @@
+/**
+ * Centralized CORS configuration for Firebase Functions
+ * Update this list when adding new authorized domains
+ */
+export const ALLOWED_ORIGINS = [
+	// Production
+	'https://app.wizcol.com',
+	'https://wizcol-app.web.app',
+	// Development/Testing
+	'https://freedi-app-dev.web.app',
+	'https://freedi-app-dev.firebaseapp.com',
+	// Local development
+	'http://localhost:5173',
+	'http://localhost:5174',
+	'http://localhost:5175',
+	'http://localhost:5176',
+	'http://localhost:3000'
+] as const;
+
+export type AllowedOrigin = typeof ALLOWED_ORIGINS[number];
