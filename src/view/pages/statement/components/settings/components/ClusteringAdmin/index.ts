0226e164613fa01fe8e38a3940b4e72aeec9dff0
Tal Yaron <tal.yaron@gmail.com>
2025-12-23 08:09:47 +0200

    forntend is ready

---
+++ src/view/pages/statement/components/settings/components/ClusteringAdmin/index.ts
@@ -0,0 +1,5 @@
+export { default as ClusteringAdmin } from './ClusteringAdmin';
+export { default as FramingList } from './FramingList';
+export { default as FramingDetail } from './FramingDetail';
+export { default as ClusterCard } from './ClusterCard';
+export { default as RequestFramingModal } from './RequestFramingModal';
