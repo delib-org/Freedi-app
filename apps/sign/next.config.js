0499e4089d4ce6e0eedf9d785b1e5ea28d47c2c4
Tal Yaron <tal.yaron@gmail.com>
2026-01-14 13:42:41 +0200

    remove sentry from sign - it cause erros

--- apps/sign/next.config.js
+++ apps/sign/next.config.js
@@ -1,5 +1,3 @@
-const { withSentryConfig } = require('@sentry/nextjs');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -50,49 +48,7 @@ const nextConfig = {
   // Experimental features
   experimental: {
     optimizePackageImports: ['delib-npm'],
-    // Enable instrumentation for Sentry
-    instrumentationHook: true,
   },
 };
 
-// Sentry configuration options
-// Note: Source map upload and release creation are disabled because
-// the sign app doesn't have its own Sentry project yet.
-// To enable: create a Sentry project for sign and set SENTRY_PROJECT_SIGN in Vercel
-const sentryWebpackPluginOptions = {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT_SIGN || 'freedi-sign',
-
-  // Disable source map upload - sign doesn't have its own Sentry project yet
-  sourcemaps: {
-    disable: true,
-  },
-
-  // Disable release creation - sign doesn't have its own Sentry project yet
-  release: {
-    create: false,
-  },
-
-  // Disable telemetry
-  telemetry: false,
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // Upload a larger set of source maps for prettier stack traces
-  widenClientFileUpload: true,
-
-  // Automatically annotate React components
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements
-  disableLogger: true,
-};
-
-// Export configuration wrapped with Sentry
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+module.exports = nextConfig;
